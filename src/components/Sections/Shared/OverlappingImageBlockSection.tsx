900ae77bc37536ad5bc42464dfda3a082bbe0412
gabrielzleonardo <gleonardoalano@gmail.com>
2025-12-09 11:03:53 -0300

    fix: image align

--- src/components/Sections/Shared/OverlappingImageBlockSection.tsx
+++ src/components/Sections/Shared/OverlappingImageBlockSection.tsx
@@ -43,7 +43,7 @@ export function OverlappingImageBlockSection({
         theme="secondary"
         className="container"
       >
-        <div className="relative mx-auto -mb-16 aspect-video max-h-[250px] overflow-hidden rounded-[20px] shadow-[6px_6px_12px_0_rgba(0,0,0,0.24)] lg:-mb-32 lg:max-h-[500px] lg:rounded-[40px] lg:shadow-[12px_12px_24px_0_rgba(0,0,0,0.24)]">
+        <div className="relative mx-auto aspect-video max-h-[250px] translate-y-1/2 overflow-hidden rounded-[20px] shadow-[6px_6px_12px_0_rgba(0,0,0,0.24)] lg:-mb-32 lg:max-h-[500px] lg:rounded-[40px] lg:shadow-[12px_12px_24px_0_rgba(0,0,0,0.24)]">
           <Image
             src={image.src}
             alt={image.alt}
@@ -59,7 +59,7 @@ export function OverlappingImageBlockSection({
         as="div"
         logoMobile="icon-full-white"
         logoDesktop="icon-full-white"
-        className={cn('pt-24 pb-16 lg:pt-44 lg:pb-20', bgVariants[variant])}
+        className={cn('pt-36 pb-16 lg:pt-44 lg:pb-20', bgVariants[variant])}
         theme="default"
       >
         <div className="container flex flex-col space-y-5 lg:space-y-10">
